88cf2d813e426c2615ab08cf224a6d1a770700cd
Spencer1923 <spencer24ss9@gmail.com>
2026-07-29 12:31:36 -0400

    updated version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
                 
                 {/* projects me laptop icon*/}
                 <a href='#projects' className='header-anchor'>
-                    <i className="fa-regular fa-laptop-code header-icons"></i>
+                    <i className="fa-solid fa-laptop-code header-icons"></i>
                 </a>
             </div>
       
@@ -28,7 +28,7 @@ export default function Header() {
          <a href='#top' className='playstation-nav-button'><i className="fa-solid fa-id-card"></i></a>
         <a href='#about-me' className='playstation-nav-button'><i className='fa-solid fa-user-tie header-icons'></i></a>
         <a href='#experience' className='playstation-nav-button'><i className='fa-solid fa-briefcase header-icons'></i></a>
-        <a href='#projects' className='playstation-nav-button'><i className="fa-regular fa-laptop-code header-icons"></i></a>
+        <a href='#projects' className='playstation-nav-button'><i className="fa-solid fa-laptop-code header-icons"></i></a>
       </div>
     </header>
   )
