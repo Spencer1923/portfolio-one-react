ba6e5294e5f5c8d41d715eb4bc19d422bc2079e2
Spencer1923 <spencer24ss9@gmail.com>
2026-07-29 12:40:46 -0400

    updated version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
                 
                 {/* projects me laptop icon*/}
                 <a href='#projects' className='header-anchor'>
-                    <i className="fa-solid fa-laptop header-icons"></i>
+                    <i className="fa-solid fa-headphones-simple header-icons"></i>                
                     
                     {/*<i className="fa-solid fa-laptop-code header-icons"></i> */}
                 </a>
